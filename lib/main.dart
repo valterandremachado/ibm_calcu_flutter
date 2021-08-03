7105475a93a12f591acac22519c4722b6b0ce5dc
Valter Machado VM <valterandremachadodinho@gmail.com>
2021-08-03 13:53:08 +0800

    added theme and refactored main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,24 @@
 import 'package:flutter/material.dart';
+import 'input_view.dart';
 
 void main() {
   runApp(BMICalculatorApp());
 }
 
+// Primary view
 class BMICalculatorApp extends StatelessWidget {
   const BMICalculatorApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MainView(),
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Colors.cyan[700],
+        scaffoldBackgroundColor: Colors.cyan[500],
+      ),
+      home: InputView(),
     );
   }
 }
 
-class MainView extends StatefulWidget {
-  const MainView({Key? key}) : super(key: key);
 
-  @override
-  _MainViewState createState() => _MainViewState();
-}
-
-class _MainViewState extends State<MainView> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI'),
-      ),
-      body: Center(
-        child: Text('This is the body'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: null,
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
